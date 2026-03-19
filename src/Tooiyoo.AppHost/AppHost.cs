855dc36aa55e51a820dd339907fd83efa97f1550
Alejandro Sanchez <alejandro.sanchez.bastida@gmail.com>
2026-03-19 10:49:19 +0100

    Remove comments

--- src/Tooiyoo.AppHost/AppHost.cs
+++ src/Tooiyoo.AppHost/AppHost.cs
@@ -2,7 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var kurrentDb =
     builder.AddKurrentDB("kurrent", 2113)
-        //.WithDataVolume()
         .WithLifetime(ContainerLifetime.Session)
         .WithEnvironment("KURRENTDB_CLUSTER_SIZE", "1")
         .WithEnvironment("KURRENTDB_RUN_PROJECTIONS", "All")
@@ -14,7 +13,6 @@ var kurrentDb =
 var mongoDb =
         builder.AddMongoDB("mongo", 27017)
             .WithEnvironment("GLIBC_TUNABLES", "libc.cpu.hwcaps=-SHSTK")
-        //.WithDataVolume()
         .WithLifetime(ContainerLifetime.Session)
         .WithMongoExpress(cfg => cfg.WithHostPort(27018));
 
