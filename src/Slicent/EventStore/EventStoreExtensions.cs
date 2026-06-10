9d4ed434fc02605841966cae923772dd80c16baf
Diego Martin <martind@vitessepsp.com>
2026-06-10 15:45:08 +0200

    #1 Fixed idempotency problem when completing profile a second timie and no changes to persist

--- src/Slicent/EventStore/EventStoreExtensions.cs
+++ src/Slicent/EventStore/EventStoreExtensions.cs
@@ -15,7 +15,7 @@ public static class EventStoreExtensions
         where TState : State<TState>, new()
         where TId : Id =>
         await eventReader.LoadAggregate<TAggregate, TState, TId>(id, failIfNotFound: false, cancellationToken: ct);
-    
+
     public static Task<AppendEventsResult[]> Store<
         TAggregate1, TState1, TId1,
         TAggregate2, TState2, TId2>(
@@ -28,19 +28,40 @@ public static class EventStoreExtensions
         where TId1 : Id
         where TAggregate2 : Aggregate<TState2>
         where TState2 : State<TState2, TId2>, new()
-        where TId2 : Id {
-
+        where TId2 : Id
+    {
+        ArgumentNullException.ThrowIfNull(eventWriter);
         ArgumentNullException.ThrowIfNull(aggregate1);
         ArgumentNullException.ThrowIfNull(aggregate2);
-        
-        var streamOne = StreamNameFactory.For<TAggregate1, TState1, TId1>(aggregate1.State.Id);
-        var streamTwo = StreamNameFactory.For<TAggregate2, TState2, TId2>(aggregate2.State.Id);
 
-        return eventWriter.Store(
-            [
-                (streamOne, new ExpectedStreamVersion(aggregate1.OriginalVersion), aggregate1.Changes),
-                (streamTwo, new ExpectedStreamVersion(aggregate2.OriginalVersion), aggregate2.Changes)
-            ],
-            cancellationToken: ct);
+        var streams =
+            new List<(StreamName StreamName, ExpectedStreamVersion ExpectedVersion, IReadOnlyCollection<object> Events
+                )>();
+
+        if (aggregate1.Changes.Count > 0)
+        {
+            var streamOne = StreamNameFactory.For<TAggregate1, TState1, TId1>(aggregate1.State.Id);
+
+            streams.Add((
+                streamOne,
+                new ExpectedStreamVersion(aggregate1.OriginalVersion),
+                aggregate1.Changes
+            ));
+        }
+
+        if (aggregate2.Changes.Count > 0)
+        {
+            var streamTwo = StreamNameFactory.For<TAggregate2, TState2, TId2>(aggregate2.State.Id);
+
+            streams.Add((
+                streamTwo,
+                new ExpectedStreamVersion(aggregate2.OriginalVersion),
+                aggregate2.Changes
+            ));
+        }
+
+        return streams.Count == 0
+            ? Task.FromResult(Array.Empty<AppendEventsResult>())
+            : eventWriter.Store(streams, cancellationToken: ct);
     }
 }
